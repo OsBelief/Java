947ffeee35ebc0d0af4253d1696ce49953bc59f5
Yicha_chengzhenhua <tianzhihen5236@gmail.com>
2013-12-02 14:09:19 +0800

    synchronized和ReentrantLock的使用

--- src/com/java/extension/ConsistentHash.java
+++ src/com/java/extension/ConsistentHash.java
@@ -6,12 +6,12 @@ import java.util.Collection;
 import java.util.SortedMap;
 import java.util.TreeMap;
 /**
- * 一致性hash算法
+ * 一致性哈希
  * @author Belief
  * @param <T>
  */
 public class ConsistentHash<T> {
-	private static MessageDigest md;
+	private static MessageDigest md;	//md5哈希算法
 	static {
 		try {
 			md = MessageDigest.getInstance("MD5");
@@ -19,24 +19,41 @@ public class ConsistentHash<T> {
 			e.printStackTrace();
 		}
 	}
-	private int numberOfReplicas;	//虚拟节点的个数
+	
+	private int numberOfReplicas;	//虚拟节点个数
 	private SortedMap<Integer, T> circle = new TreeMap<Integer, T>();
+	
 	public ConsistentHash(int numerOfReplicas, Collection<T> nodes) {
 		this.numberOfReplicas = numerOfReplicas;
 		for(T node : nodes) {
 			add(node);
 		}
 	}
+	/**                                                                                     
+	 * 添加节点
+	 * @param node
+	 */
 	public void add(T node) {
 		for(int i = 0; i < numberOfReplicas; ++i) {
 			circle.put(this.hash(node.toString() + i), node);
 		}
 	}
+	/**
+	 * 删除节点
+	 * @param node
+	 */
 	public void remove(T node) {
-		for(int i = 0; i < numberOfReplicas; ++i) {
-			circle.remove(this.hash(node.toString() + i));
+		int i = 0;
+		//删除所有的虚拟节点
+		while(circle.remove(this.hash(node.toString() + i)) != null) {
+			++i;
 		}
 	}
+	/**
+	 * 为对象获得最近的一个顺时针节点
+	 * @param key
+	 * @return
+	 */
 	public T get(Object key) {
 		if(circle.isEmpty()) {
 			return null;
@@ -44,15 +61,30 @@ public class ConsistentHash<T> {
 		int hash = this.hash(key.toString());
 		if(!circle.containsKey(hash)) {
 			SortedMap<Integer, T> tailMap = circle.tailMap(hash);
+			//由于是circle,则没有比Object的hash大的,则循环到第一个
 			hash = tailMap.isEmpty() ? circle.firstKey() : tailMap.firstKey();
 		}
+		//正好命中
 		return circle.get(hash);
 	}
-	//哈希算法
-	public int hash(String node) {
+	/**
+	 * 哈希算法
+	 * @param node
+	 * @return
+	 */
+	public Integer hash(String node) {
 		md.reset();
 		md.update(node.getBytes());
-		byte[] buffer = md.digest();
-		return Integer.parseInt(buffer.toString());
+		byte[] temp = md.digest();
+		//截取中间32位转换为整数
+		int reslut = (temp[6] & 0xff) | ((temp[7] << 8) & 0xff00)
+				| ((temp[8] << 24) >>> 8) | (temp[9] << 24); 
+		return Integer.valueOf(Math.abs(reslut)); 
+	}
+	public int getNumberOfReplicas() {
+		return numberOfReplicas;
+	}
+	public void setNumberOfReplicas(int numberOfReplicas) {
+		this.numberOfReplicas = numberOfReplicas;
 	}
 }
