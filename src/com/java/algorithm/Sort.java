6c45a4706bee5cd5dd172a5ae02ff449ef59e596
chengzh <chengzh@ushareit.com>
2019-05-09 09:11:53 +0800

    冒泡排序的优化

--- src/com/java/algorithm/Sort.java
+++ src/com/java/algorithm/Sort.java
@@ -53,6 +53,33 @@ public class Sort {
     }
 
     /**
+     * 冒泡排序的优化
+     * @param array
+     */
+    public static void bubbleSort2(int array[]) {
+        int tmp = 0;
+        boolean isSorted = true;
+        int lastExchangeIndex = 0;    //记录最后一次交换的位置
+        int sortBorder = array.length - 1;//无序数列的边界，每次比较只需要比到这里为止
+        for (int i = 0; i < array.length; i++) {        //有序标记，每一轮的初始是true
+            for (int j = 0; j < sortBorder; j++) {
+                if (array[j] > array[j + 1]) {
+                    tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                    //有元素交换，所以不是有序，标记变为false
+                    isSorted = false;
+                    lastExchangeIndex = j;//把无序数列的边界更新为最后一次交换元素的位置
+                }
+            }
+            if (isSorted) {
+                break;
+            }
+            sortBorder = lastExchangeIndex;
+        }
+    }
+
+    /**
      * 直接插入排序<br>
      * 基本思想:直接插入排序是将无序区的第一个元素直接插入到有序区以形成一个更大的有序区，
      * 而直接选择排序是从无序区选一个最小的元素直接放到有序区的最后。
